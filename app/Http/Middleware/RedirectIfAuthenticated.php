c826c6b8d1dc1b9ee907fed4bd67316410b2d2ab
kennyLtv <kenny@kennyl.tv>
2017-12-27 11:55:29 -0500

    CircleCI and Redirect based on guard.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,14 +18,14 @@ class RedirectIfAuthenticated
         switch(Auth::guard($guard)->check()){
             case 'user':
                 return redirect()->route('user_dashboard');
-            case 'superadmin':
-                return redirect()->route('admin_dashboard');
-            case 'admin':
-                return redirect()->route('admin_dashboard');                
             case 'support':
                 return redirect()->route('admin_dashboard'); 
+            case 'admin':
+                return redirect()->route('admin_dashboard');                
+            case 'superadmin':
+                return redirect()->route('admin_dashboard');
             default:
-             return $next($request);
+                return $next($request);
 
         }
         return $next($request);
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,12 @@ const { mix } = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+    
+mix.sass('resources/assets/sass/app.scss', 'public/css')
 
-mix.js('resources/assets/js/User/index.js', 'public/js/users/app.js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    // Site JS
+    .js('resources/assets/js/index.js', 'public/js/bundle.js')
+    //User SPA
+    .js('resources/assets/js/User/index.js', 'public/js/users/app.js')
+    // Admin SPA
+    .js('resources/assets/js/Admin/index.js', 'public/js/admin/app.js')
